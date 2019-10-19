0bb4e6f7cf70e4deb22078d5b10d3bb0bc8bdbf6
Ha Pham <hapham0392@gmail.com>
2019-10-19 00:15:04 +0000

    update seed file

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -9,7 +9,6 @@ VALUES (2, 'Dining', 'Places I ate at today', true),
 VALUES (3, 'Rare animal sightings', 'White squirrels discovered', false);
 
 INSERT INTO pins (id, comment, latitude, longitude)
-VALUES (1, 'George Clooney spotting', 43°38'51.8, -79°23'43.6),
-VALUES (2, 'White Squirrel spotting', 43°37'59.1, -79°25'28.7),
-VALUES (3, 'Oretta', 43°38'37.6, -79°24'06.2);
-
+VALUES (1, 'George Clooney spotting', 43.38518, -79.2343),
+VALUES (2, 'White Squirrel spotting', 43.37591, -79.2528),
+VALUES (3, 'Oretta', 43.3837, -79.2406);
