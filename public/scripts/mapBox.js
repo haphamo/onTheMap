5f2d68c489291c4d3e4b0cdc92808332a0cb8b64
Ha Pham <hapham0392@gmail.com>
2019-10-19 16:44:39 +0000

    added another page on the page

--- public/scripts/mapBox.js
+++ public/scripts/mapBox.js
@@ -1,5 +1,5 @@
 //initialize MAP
-let map;
+let map, map2;
 function initMap() {
   //map options
   let options = {
@@ -8,28 +8,29 @@ function initMap() {
   }
   //new map
   map = new google.maps.Map(document.getElementById('map'), options);
+  map2 = new google.maps.Map(document.getElementById('map2'), options);
 
-  //Listen for clicks for new markers
-
+  //Listen for clicks to add markers for map
   google.maps.event.addListener(map, 'click', function(event){
     addMarker({coords:event.latLng});
-    //Add marker
   })
 
-
-  let markers = [{coords: { lat: 42.7652, lng: -80.5433 }}, {coords: { lat: 42.9332, lng: -80.6643 }}];
-
- for (let marker in markers) {
+  //hard coded markers
+  let markers = [{coords: { lat: 43.6532, lng: -79.3832 }}, {coords: { lat: 43.5890, lng: -79.6441 }}];
+  //created a loop for add markers for given latlng
+  for (let marker in markers) {
    addMarker(markers[marker]);
-
- }
- marker.addListener('click', function(){
+  }
+  //click listener to add markers
+  marker.addListener('click', function(){
     infoWindow.open(map, marker)
   })
+
   function addMarker(props){
     let marker = new google.maps.Marker({
     position: props.coords,
-    map:map
+    map:map,
+
 
   });
   }
