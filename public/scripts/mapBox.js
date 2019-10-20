081aa0976aba9e9ad7c017a6e9586eab9b355a54
Ha Pham <hapham0392@gmail.com>
2019-10-20 14:28:50 +0000

    removed spaces on line 36 on users page and trying to create maps and markers incrementally on the mapbox

--- public/scripts/mapBox.js
+++ public/scripts/mapBox.js
@@ -1,37 +1,55 @@
+//A hardcoded sample database
+
+let pinsOfMap1 = [{ comment: 'Toronto', coords: {lat: 43.6532, lng: -79.3832 }}, { comment: 'Missisauga', coords: {lat: 43.5890, lng: -79.6441 }}];
+
+let pinsOfMap2 = [{ comment: 'Vaughan', coords: {lat: 43.8563, lng: -79.5085 }}, { comment: 'Richmond Hill', coords: {lat: 43.8828, lng: -79.4403 }}];
+
 //initialize MAP
-let map;
 function initMap() {
   //map options
   let options = {
     center: {lat: 43.6532, lng: -79.3832},
     zoom: 8
   }
-  //new map
-  map = new google.maps.Map(document.getElementById('map'), options);
-
-  //Listen for clicks for new markers
-
+  //creating maps
+  let map = new google.maps.Map(document.getElementById('map'), options);
+  let map2 = new google.maps.Map(document.getElementById('map2'), options);
+  //Listen for clicks for new markers for map
   google.maps.event.addListener(map, 'click', function(event){
     addMarker({coords:event.latLng});
-    //Add marker
   })
-
-
-  let markers = [{coords: { lat: 42.7652, lng: -80.5433 }}, {coords: { lat: 42.9332, lng: -80.6643 }}];
-
- for (let marker in markers) {
-   addMarker(markers[marker]);
-
+  google.maps.event.addListener(map2, 'click', function(event){
+    addMarker({coords:event.latLng});
+  })
+  let createMarkersOnMap1 = new google.maps.Marker({
+    position: pinsOfMap1[0].coords,
+    map:map
+  })
+  let createMarkersOnMap2 = new google.maps.Marker({
+    position: pinsOfMap1[1].coords,
+    map:map2
+  })
+ for (let pin of pinsOfMap1) {
+   addMarkerForMap1(pinsOfMap1[pin].coords);
  }
- marker.addListener('click', function(){
-    infoWindow.open(map, marker)
-  })
-  function addMarker(props){
+ for (let pin of pinsOfMap2) {
+  addMarkerForMap2(pinsOfMap2[pin].coords);
+}
+
+//  marker.addListener('click', function(){
+//     infoWindow.open(map, marker)
+//   })
+  function addMarkerForMap1(props){
     let marker = new google.maps.Marker({
     position: props.coords,
     map:map
-
   });
   }
+  // function addMarkerForMap2(props){
+  //   let marker = new google.maps.Marker({
+  //   position: props.coords,
+  //   map:map2
+  // });
+  // }
 
 }
