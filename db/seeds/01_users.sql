e5df6fc7cb03fcec70959eba9e13bf4ac070043f
Ha Pham <hapham0392@gmail.com>
2019-10-21 19:30:19 +0000

    Updated the database to start with one user and the data type for lat and lng values are correct now

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,14 +1,12 @@
 INSERT INTO users (id, full_name, email, password)
-VALUES (1, 'Avijit Choudhury', 'avijit.choudhury24@gmail.com', 'password'),
-(2, 'Ha Pham', 'hapham0392@gmail.com', 'password'),
-(3, 'Jordan Owens', 'jordanowens86@gmail.com', 'password');
+VALUES (1, 'Avijit Choudhury', 'avijit.choudhury24@gmail.com', 'password')
 
-INSERT INTO maps (id, title, description, is_favorite)
-VALUES (1, 'Travel adventures', 'Places I have travelled to', true),
-(2, 'Dining', 'Places I ate at today', true),
-(3, 'Rare animal sightings', 'White squirrels discovered', false);
+INSERT INTO maps (id, user_id, title, description, is_favorite)
+VALUES (1, 1, 'Cities in the GTA', 'There are where my friends live', true)
 
-INSERT INTO pins (id, comment, latitude, longitude)
-VALUES (1, 'George Clooney spotting', 43.38518, -79.2343),
-(2, 'White Squirrel spotting', 43.37591, -79.2528),
-(3, 'Oretta', 43.3837, -79.2406);
+
+INSERT INTO pins (id, map_id, comment, latitude, longitude)
+VALUES (1, 1, 'Toronto', 43.6532, -79.3832),
+(2, 1, 'Missisauga', 43.6532, -79.3832),
+(3, 1, 'Vaughan', 43.6532, -79.3832),
+(4, 1, 'Richmond Hill', 43.6532, -79.3832)
