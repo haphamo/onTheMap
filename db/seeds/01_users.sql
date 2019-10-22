6c05b1c723c658db9c590b96453c179a490c4d85
Ha Pham <hapham0392@gmail.com>
2019-10-22 18:08:37 +0000

    added search bar to  the map

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -7,6 +7,6 @@ VALUES (1, 1, 'Cities in the GTA', 'There are where my friends live', true)
 
 INSERT INTO pins (id, map_id, comment, latitude, longitude)
 VALUES (1, 1, 'Toronto', 43.6532, -79.3832),
-(2, 1, 'Missisauga', 43.6532, -79.3832),
-(3, 1, 'Vaughan', 43.6532, -79.3832),
-(4, 1, 'Richmond Hill', 43.6532, -79.3832)
+(2, 1, 'Missisauga', 43.5890, -79.6441),
+(3, 1, 'Vaughan', 43.8563, -79.5085),
+(4, 1, 'Richmond Hill', 43.8828, -79.4403)
