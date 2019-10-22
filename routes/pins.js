8c60c93aa458827633d457283f47b7b2033400fd
Your Name <you@example.com>
2019-10-22 19:23:58 +0000

    tinkered

--- routes/pins.js
+++ routes/pins.js
@@ -10,7 +10,9 @@ module.exports = (db) => {
     // db.query(`SELECT * FROM pins;`)
     db.query(`SELECT pins.*
               FROM pins
-              JOIN maps ON maps.id = pins.map_id `)
+              JOIN maps ON maps.id = pins.map_id
+              JOIN users ON users.id = maps.user_id
+              WHERE users.id = maps.user_id `)
     .then(data => data.rows.map(pin => {
       return {comment: pin.comment,
               coords: {lat: Number(pin.latitude),
