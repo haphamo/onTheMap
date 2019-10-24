5804e17d7561e3dbceac869ffd39f7ce30c8b378
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-10-24 16:15:28 +0000

    modified styles with new font

--- public/scripts/mapBox.js
+++ public/scripts/mapBox.js
@@ -106,7 +106,7 @@ function initMap() {
 
 $(document).ready(function () {
 
-  $("#slide-toggle").on('click', function () {
+  $(".btn").on('click', function () {
     $(".nav_div").slideToggle('fast', function () {
       if ($(this).is(':visible'))
       $(this).css('display', 'flex');
